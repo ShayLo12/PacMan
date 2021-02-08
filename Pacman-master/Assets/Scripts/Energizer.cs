c2f9092097783c674cc6f54bfebf8d2f223eb6ae
Yurrushia <66466727+Yurrushia@users.noreply.github.com>
2021-02-08 16:17:29 -0500

    add pick up sfx

--- Pacman-master/Assets/Scripts/Energizer.cs
+++ Pacman-master/Assets/Scripts/Energizer.cs
@@ -1,23 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Energizer : MonoBehaviour {
+public class Energizer : MonoBehaviour
+{
 
     private GameManager gm;
 
-	// Use this for initialization
-	void Start ()
-	{
-	    gm = GameObject.Find("Game Manager").GetComponent<GameManager>();
-        if( gm == null )    Debug.Log("Energizer did not find Game Manager!");
-	}
+    AudioSource audioSource;
+    public AudioClip pickupSound;
+
+    // Use this for initialization
+    void Start()
+    {
+        gm = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        if (gm == null) Debug.Log("Energizer did not find Game Manager!");
+        audioSource = GetComponent<AudioSource>();
+    }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if(col.name == "pacman")
+        if (col.name == "pacman")
         {
+            AudioSource.PlayClipAtPoint(pickupSound, transform.position);
             gm.ScareGhosts();
-            Destroy(gameObject);
+            gameObject.SetActive(false);
         }
     }
+
+
 }
